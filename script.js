2136350717d7f16d8fbbc187e3a0ca46a51a6d7b
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-09-28 10:19:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let btn = document.querySelector("#shorten-btn"),
+  menu = document.querySelector(".shorted--links");
+
+const compose = (f, g) => (data) => g(f(data));
+
+const shortLink = async function (data) {
+  try {
+    const response = await fetch(
+      `https://api.shrtco.de/v2/shorten?url=${data}`
+    );
+    let link = await response.json();
+    //console.log(link.result.full_short_link)
+    return link;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+const createLi = (x) => {
+  x.then((a) => {
+
+    let newLi = document.createElement("li");
+    newLi.classList.add("shorted--link");
+    newLi.innerHTML = `
+        <span>${a.result.original_link}</span>
+        <span><a href="#">${a.result.full_short_link}</a>
+        <a href="${a.result.full_short_link}" class="btn">copy</a></span>
+        `;
+    menu.appendChild(newLi);
+  });
+
+};
+const shortAndAdd = compose(shortLink, createLi);
+btn.addEventListener("click", function () {
+    shortAndAdd(document.querySelector("#shorten").value);
+  });
+//shortAndAdd(document.querySelector("#shorten").value)
+
+
+
+// const shortLink = async function(y){
+//     try {
+//         const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${y}`);
+//         let link = await response.json()
+//         //console.log(link.result.full_short_link)
+//         console.log( link.result.full_short_link)
+//     }catch (err){
+//         console.log(err)
+//     }
+// }
+
+// const createLi = (x) => {
+
+//     let newLi = document.createElement("li")
+//     newLi.classList.add("shorted--link")
+//     newLi.innerHTML= `
+//     <span>frontendmentor.io</span>
+//     <span><a href="#">${x}</a>
+//     <a href="${x}" class="btn">copy</a></span>
+//     `;
+//     menu.appendChild(newLi)
+
+// }
