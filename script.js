1eeef02c74f7931f35732680a751044cef088983
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-09-29 10:45:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector("#shorten-btn"),
-  menu = document.querySelector(".shorted--links");
+  menu = document.querySelector(".shorted--links"),
+  btnC = document.querySelectorAll(".btn-c");
 
 const compose = (f, g) => (data) => g(f(data));
 
@@ -9,29 +10,48 @@ const shortLink = async function (data) {
       `https://api.shrtco.de/v2/shorten?url=${data}`
     );
     let link = await response.json();
-    //console.log(link.result.full_short_link)
     return link;
   } catch (err) {
     console.log(err);
   }
 };
 
-
 const createLi = (x) => {
+  liQuery = document.querySelectorAll(".shorted--link");
   x.then((a) => {
-
+    
     let newLi = document.createElement("li");
     newLi.classList.add("shorted--link");
+
     newLi.innerHTML = `
         <span>${a.result.original_link}</span>
-        <span><a href="#">${a.result.full_short_link}</a>
-        <a href="${a.result.full_short_link}" class="btn">copy</a></span>
+        <span><a href="${a.result.full_short_link}" target="_blank" class="copied-link"><input type="text" name="link" class="shortlink" value="${a.result.full_short_link}">${a.result.full_short_link} </a>
+        <a class="btn btn-c">copy</a></span>
         `;
-    menu.appendChild(newLi);
-  });
 
-};
-const shortAndAdd = compose(shortLink, createLi);
-btn.addEventListener("click", function () {
-    shortAndAdd(document.querySelector("#shorten").value);
+    if (liQuery.length === 3) {
+      liQuery[0].parentNode.removeChild(liQuery[0]);
+      menu.appendChild(newLi);
+    } else {
+      menu.appendChild(newLi);
+    }
   });
+};
+
+const shortAndAdd = compose(shortLink, createLi);
+
+btn.addEventListener("click", function () {
+  shortAndAdd(document.querySelector("#shorten").value);
+
+  setTimeout(function () {
+    document.querySelectorAll(".btn-c").forEach(function (a, index) {
+      let x = document.querySelectorAll(".shortlink");
+
+      a.addEventListener("click", function () {
+        a.classList.add("copied");
+        x[index].select();
+        navigator.clipboard.writeText(x[index].value);
+      });
+    });
+  }, 1000);
+});
